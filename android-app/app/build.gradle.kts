5ee47febd51f1a0a3ee6dda67aadc4e3115dbed7
ianlyoo <torch0412@naver.com>
2026-09-05 15:20:52 +0900

    chore(release): prepare v1.7.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.koreainv.dashboard"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.7.1"
+        versionCode = 31
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
